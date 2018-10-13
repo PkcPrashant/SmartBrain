d98cbcdd32f35f586905550f1849f4c296fe5a23
PkcPrashant <pc22230@gmail.com>
2018-10-14 03:05:50 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -77,7 +77,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-      fetch('https://pkcprashant.github.io/smartbrain-api/imageurl', {
+      fetch('https://safe-dusk-91692.herokuapp.com/imageurl', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
@@ -87,7 +87,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('https://pkcprashant.github.io/smartbrain-api/image', {
+          fetch('https://safe-dusk-91692.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('https://pkcprashant.github.io/smartbrain-api/register', {
+    fetch('https://safe-dusk-91692.herokuapp.com/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
